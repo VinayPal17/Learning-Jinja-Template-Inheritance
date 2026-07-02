1092704b3aefbad23fe27a96ff638a3277685056
VinayPal17 <palv04565@gmail.com>
2026-07-02 11:14:05 +0530

    Apply CSS and and fix template inheritance

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -14,4 +14,5 @@ def about():
 def contact():
     return render_template("contact.html")      
 
-app.run(debug=True)    
+if __name__ == "__main__":
+    app.run(debug=True)    
